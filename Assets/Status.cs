5b0613f213244ff9a521fc292519b8f9526a0fa8
Kwannoo <kwansing2000@hotmail.nl>
2021-10-31 16:20:13 +0100

    hci start

---
+++ Assets/Status.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour
+{
+    public bool hasBomb = false;
+    public bool isDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
